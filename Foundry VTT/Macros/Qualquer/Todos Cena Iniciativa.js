53f5ce0a6bb1d144bc553944cd380fd233037eb8
Bruno Calado <brunocalado@users.noreply.github.com>
2020-08-05 22:21:07 -0300

    iniciativa

---
+++ Foundry VTT/Macros/Qualquer/Todos Cena Iniciativa.js
@@ -0,0 +1,32 @@
+//// modified from @atnoslen with a little help from @Atropos (thanks!) 
+const scene = game.scenes.entities.filter(scene => scene.active === true)[0];
+
+let tokens = [];
+
+let startCRoll=function rollAndStart(){
+  game.combat.rollAll();
+  game.combat.startCombat();
+}
+
+if (!game.combat){
+  scene.data.tokens.forEach(function(token) {
+    tokens.push({tokenId:token._id});
+  });
+
+  Combat.create({scene:scene.data._id, combatants:tokens}).then(startCRoll);
+
+} else {
+  // Combat already exists, add the missing tokens.
+  // This assumes createCombatant is expensive, so create an array
+  // instead of calling individually.
+
+  scene.data.tokens.forEach(function(token) {
+    if (game.combat.combatants.filter(combatant => combatant.tokenId === token._id).length === 0) {
+      tokens.push({tokenId:token._id});
+    }
+  });
+
+  game.combat.createCombatant(tokens).then(startCRoll);  
+
+}
+
