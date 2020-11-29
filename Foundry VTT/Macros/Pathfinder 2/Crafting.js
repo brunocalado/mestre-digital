20af559761395ca9001d761124b4b7185c7c715a
adfs <brunocaladoribeiro@yahoo.com.br>
2020-11-29 00:24:37 -0300

    v2

--- Foundry VTT/Macros/Pathfinder 2/Crafting.js
+++ Foundry VTT/Macros/Pathfinder 2/Crafting.js
@@ -1,56 +1,91 @@
-const version = 'v0.1';
-/* Crafting v0.1
+const version = 'v0.2';
+/* Crafting
 Features
-- s
+- Mostra valor da fórmula
+
+TODO
+- A macro parece só faltar o valor da formula que é fixa. Pode colocar um checkbox (incluir preço da formula)
+
+Rules
+Diviner's Sight
+https://2e.aonprd.com/Spells.aspx?ID=525
 
 Source: https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Foundry%20VTT/Macros/Pathfinder%202/Crafting.js
 Icon: icons/tools/smithing/crucible.webp
 */
 
+if (!actor) {
+  ui.notifications.warn(`Selecione um token!`); // get selected token 
+  return;
+} 
+
+// 
+const tableFormulas = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 5,
+  5: 8,
+  6: 13,
+  7: 18,
+  8: 25,
+  9: 35,
+  10: 50,
+  11: 70,
+  12: 100,
+  13: 150,
+  14: 225,
+  15: 325,
+  16: 500,
+  17: 750,
+  18: 1250,
+  19: 2000,
+  20: 3500
+};
+
 (async () => {
-let toChat = (content, rollString) => {
+  let toChat = (content, rollString) => {
     let chatData = {
-        user: game.user.id,
-        content,
-        speaker: ChatMessage.getSpeaker(),
+      user: game.user.id,
+      content,
+      speaker: ChatMessage.getSpeaker(),
     }
     ChatMessage.create(chatData, {})
     if (rollString) {
-        let roll = new Roll(rollString).roll();
-        chatData = {
-            ...chatData,
-            flavor: "Crafting Results",
-            type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-            roll
-          }
-        ChatMessage.create(chatData, {})
-    }
-    
-}
-
-const handleCrits = (roll) => roll === 1 ? -10 : (roll === 20 ? 10 : 0);
-
-let rollCrafting = (args) => {
+      let roll = new Roll(rollString).roll();
+      chatData = {
+        ...chatData,
+        flavor: "Resultado da Manufatura",
+        type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+        roll
+      }
+      ChatMessage.create(chatData, {})
+    }    
+  }
+  
+  const handleCrits = (roll) => roll === 1 ? -10 : (roll === 20 ? 10 : 0);
+  
+  let rollCrafting = (args) => {
     let {itmLevel, cra, charLevel, itmType, itmValue, charName, devinSight} = args;
-
+    
     var roll = new Roll(`d20`).roll().total;
     var roll2 = new Roll(`d20`).roll().total;
-
+    
     if (devinSight){
       console.log("Devin's Sight (Before) | Used roll :" + roll + ", Annex roll  :" +roll2);
-
+      
       if (roll2 > roll) {
         roll = roll2;
-     }
-
-       console.log("Devin's Sight (After) | Used roll :" + roll + ", Annex roll  :" +roll2);
-    }  
-
+      }
+      
+      console.log("Devin's Sight (After) | Used roll :" + roll + ", Annex roll  :" +roll2);
+    }
+    
     var crit = handleCrits(roll);
-
-
+    
+    
     var DC = 0, taskLevel = charLevel, gp = 0;
-
+    
     switch (itmLevel) {
       case 1:
         DC = 15;
@@ -115,12 +150,12 @@ let rollCrafting = (args) => {
       default:
         DC = 14;
         break;
-
+      
     }
-
-
+    
+    
     if (roll + crit + cra.value >= DC+10) taskLevel++;
-
+    
     if (cra.rank === 4) {
       switch (taskLevel) {
         case 1:
@@ -191,266 +226,278 @@ let rollCrafting = (args) => {
           break;
       }
     } else if (cra.rank === 3) {
-        switch (taskLevel) {
-          case 1:
-            gp = 0.2;
-            break;
-          case 2:
-            gp = 0.3;
-            break;
-          case 3:
-            gp = 0.5;
-            break;
-          case 4:
-            gp = 0.8;
-            break;
-          case 5:
-            gp = 1;
-            break;
-          case 6:
-            gp = 2;
-            break;
-          case 7:
-            gp = 2.5;
-            break;
-          case 8:
-            gp = 3;
-            break;
-          case 9:
-            gp = 4;
-            break;
-          case 10:
-            gp= 6;
-            break;
-          case 11:
-            gp = 8;
-            break;
-          case 12:
-            gp = 10;
-            break;
-          case 13:
-            gp = 15;
-            break;
-          case 14:
-            gp = 20;
-            break;
-          case 15:
-            gp = 28;
-            break;
-          case 16:
-            gp = 36;
-            break;
-          case 17:
-            gp = 45;
-            break;
-          case 18:
-            gp = 70;
-            break;
-          case 19:
-            gp = 100;
-            break;
-          case 20:
-            gp = 150;
-            break;
-          case 21:
-            gp = 175;
-            break;
-          default:
-            gp = 0.05;
-            break;
-        }
-
+      switch (taskLevel) {
+        case 1:
+          gp = 0.2;
+          break;
+        case 2:
+          gp = 0.3;
+          break;
+        case 3:
+          gp = 0.5;
+          break;
+        case 4:
+          gp = 0.8;
+          break;
+        case 5:
+          gp = 1;
+          break;
+        case 6:
+          gp = 2;
+          break;
+        case 7:
+          gp = 2.5;
+          break;
+        case 8:
+          gp = 3;
+          break;
+        case 9:
+          gp = 4;
+          break;
+        case 10:
+          gp= 6;
+          break;
+        case 11:
+          gp = 8;
+          break;
+        case 12:
+          gp = 10;
+          break;
+        case 13:
+          gp = 15;
+          break;
+        case 14:
+          gp = 20;
+          break;
+        case 15:
+          gp = 28;
+          break;
+        case 16:
+          gp = 36;
+          break;
+        case 17:
+          gp = 45;
+          break;
+        case 18:
+          gp = 70;
+          break;
+        case 19:
+          gp = 100;
+          break;
+        case 20:
+          gp = 150;
+          break;
+        case 21:
+          gp = 175;
+          break;
+        default:
+          gp = 0.05;
+          break;
+      }
+      
     } else if (cra.rank === 2) {
-        switch (taskLevel) {
-          case 1:
-            gp = 0.2;
-            break;
-          case 2:
-            gp = 0.3;
-            break;
-          case 3:
-            gp = 0.5;
-            break;
-          case 4:
-            gp = 0.8;
-            break;
-          case 5:
-            gp = 1;
-            break;
-          case 6:
-            gp = 2;
-            break;
-          case 7:
-            gp = 2.5;
-            break;
-          case 8:
-            gp = 3;
-            break;
-          case 9:
-            gp = 4;
-            break;
-          case 10:
-            gp= 5;
-            break;
-          case 11:
-            gp = 6;
-            break;
-          case 12:
-            gp = 8;
-            break;
-          case 13:
-            gp = 10;
-            break;
-          case 14:
-            gp = 15;
-            break;
-          case 15:
-            gp = 20;
-            break;
-          case 16:
-            gp = 25;
-            break;
-          case 17:
-            gp = 30;
-            break;
-          case 18:
-            gp = 45;
-            break;
-          case 19:
-            gp = 60;
-            break;
-          case 20:
-            gp = 75;
-            break;
-          case 21:
-            gp = 90;
-            break;
-          default:
-            gp = 0.05;
-            break;
-        }
-
+      switch (taskLevel) {
+        case 1:
+          gp = 0.2;
+          break;
+        case 2:
+          gp = 0.3;
+          break;
+        case 3:
+          gp = 0.5;
+          break;
+        case 4:
+          gp = 0.8;
+          break;
+        case 5:
+          gp = 1;
+          break;
+        case 6:
+          gp = 2;
+          break;
+        case 7:
+          gp = 2.5;
+          break;
+        case 8:
+          gp = 3;
+          break;
+        case 9:
+          gp = 4;
+          break;
+        case 10:
+          gp= 5;
+          break;
+        case 11:
+          gp = 6;
+          break;
+        case 12:
+          gp = 8;
+          break;
+        case 13:
+          gp = 10;
+          break;
+        case 14:
+          gp = 15;
+          break;
+        case 15:
+          gp = 20;
+          break;
+        case 16:
+          gp = 25;
+          break;
+        case 17:
+          gp = 30;
+          break;
+        case 18:
+          gp = 45;
+          break;
+        case 19:
+          gp = 60;
+          break;
+        case 20:
+          gp = 75;
+          break;
+        case 21:
+          gp = 90;
+          break;
+        default:
+          gp = 0.05;
+          break;
+      }
+      
     } else if (cra.rank === 1){
-         switch (taskLevel) {
-          case 1:
-            gp = 0.2;
-            break;
-          case 2:
-            gp = 0.3;
-            break;
-          case 3:
-            gp = 0.5;
-            break;
-          case 4:
-            gp = 0.7;
-            break;
-          case 5:
-            gp = 0.9;
-            break;
-          case 6:
-            gp = 1.5;
-            break;
-          case 7:
-            gp = 2;
-            break;
-          case 8:
-            gp = 2.5;
-            break;
-          case 9:
-            gp = 3;
-            break;
-          case 10:
-            gp= 4;
-            break;
-          case 11:
-            gp = 5;
-            break;
-          case 12:
-            gp = 6;
-            break;
-          case 13:
-            gp = 7;
-            break;
-          case 14:
-            gp = 8;
-            break;
-          case 15:
-            gp = 10;
-            break;
-          case 16:
-            gp = 13;
-            break;
-          case 17:
-            gp = 15;
-            break;
-          case 18:
-            gp = 20;
-            break;
-          case 19:
-            gp = 30;
-            break;
-          case 20:
-            gp = 40;
-            break;
-          case 21:
-            gp = 50;
-            break;
-          default:
-            gp = 0.05;
-            break;
-        }
-    }
-
-    if (itmType === "rare") DC += 5;
-
-    let message = `Starts crafting a ${itmType} item of level ${itmLevel}, supplying ${itmValue/2}gp... [[${roll}+${cra.value}]] ! </br> </br>`;
-
-    if (roll + crit + cra.value >= DC+10) {
-        toChat(`${message} Critical success ! Each additional day spent Crafting reduces the materials needed to complete the item by an amount of ${gp}gp. </br> ${charName} can also pay ${itmValue/2}gp to finish the crafting immediately or spend ${Math.ceil((itmValue/2)/gp)} additionnal days.`);
-    } else if (roll+crit + cra.value >= DC) {
-        toChat(`${message} It's a success. Each additional day spent Crafting reduces the materials needed to complete the item by an amount of ${gp}gp. </br> ${charName} can also pay ${itmValue/2}gp to finish the crafting immediately or spend ${Math.ceil((itmValue/2)/gp)} additionnal days.`,);
-    } else if (roll + crit + cra.value < DC-10) {
-        toChat(`${message} It's a critical failure... ${charName} fails to complete the item. ${charName} loses raw materials worth ${(itmValue/2)*0.1}gp but can salvage the remaining ${(itmValue/2)*0.9}gp. ${charName} must start over if he/she wants to try again.`);
-    } else if (roll+crit + cra.value < DC) {
-        toChat(`${message} It's a failure. ${charName} can salvage raw materials worth ${itmValue/2}gp. ${charName} must start over if he/she wants to try again.`);
+      switch (taskLevel) {
+        case 1:
+          gp = 0.2;
+          break;
+        case 2:
+          gp = 0.3;
+          break;
+        case 3:
+          gp = 0.5;
+          break;
+        case 4:
+          gp = 0.7;
+          break;
+        case 5:
+          gp = 0.9;
+          break;
+        case 6:
+          gp = 1.5;
+          break;
+        case 7:
+          gp = 2;
+          break;
+        case 8:
+          gp = 2.5;
+          break;
+        case 9:
+          gp = 3;
+          break;
+        case 10:
+          gp= 4;
+          break;
+        case 11:
+          gp = 5;
+          break;
+        case 12:
+          gp = 6;
+          break;
+        case 13:
+          gp = 7;
+          break;
+        case 14:
+          gp = 8;
+          break;
+        case 15:
+          gp = 10;
+          break;
+        case 16:
+          gp = 13;
+          break;
+        case 17:
+          gp = 15;
+          break;
+        case 18:
+          gp = 20;
+          break;
+        case 19:
+          gp = 30;
+          break;
+        case 20:
+          gp = 40;
+          break;
+        case 21:
+          gp = 50;
+          break;
+        default:
+          gp = 0.05;
+          break;
+      }
     }
     
-}
-
-let applyChanges = false;
-new Dialog({
-  title: `Crafting`,
-  content: `
-    <div>Select an item you want to craft. Don't forget you need to possess the according formula in order to craft this item.<div>
+    if (itmType === "rare") DC += 5;
+    
+    let message = `<h1>Manufatura</h1><p>Manufaturando um item ${itmType} de nível ${itmLevel}. O valor investido foi de ${itmValue/2}po.</p><p>O resultado da rolagem foi [[${roll}+${cra.value}]]</p>`;   
+    
+    if (roll + crit + cra.value >= DC+10) {
+      toChat(message+`<h2>Sucesso Crítico</h2> <p>Sua tentativa é bem-sucedida. Cada dia adicional gasto Manufaturando reduz os materiais necessários para completar o item em uma quantidade ${gp}po e sua graduação de proficiência. </br> ${charName} pode pagar ${itmValue/2}po para terminar imediatamente ou gastar ${Math.ceil((itmValue/2)/gp)} dias extras.</p>
+      <h3>Resumo</h3>
+      <p>Redução de Custo por Dia: ${gp}po</p>
+      <p>Custo Término Imediato: ${itmValue/2}po</p>
+      <p>Tempo para Terminar: ${Math.ceil((itmValue/2)/gp)} dias</p>
+      <p>Custo da Fórmula: ${tableFormulas[parseInt(itmLevel)]}po</p>`);
+    } else if (roll+crit + cra.value >= DC) {
+      toChat(message+`<h2>Sucesso</h2> <p>Sua tentativa é bem-sucedida. Cada dia adicional gasto Manufaturando reduz os materiais necessários para completar o item em uma quantidade ${gp}po e sua graduação de proficiência. </br> ${charName} pode pagar ${itmValue/2}po para terminar imediatamente ou gastar ${Math.ceil((itmValue/2)/gp)} dias extras.</p>
+      <h3>Resumo</h3>
+      <p>Redução de Custo por Dia: ${gp}po</p>
+      <p>Custo Término Imediato: ${itmValue/2}po</p>
+      <p>Tempo para Terminar: ${Math.ceil((itmValue/2)/gp)} dias</p>
+      <p>Custo da Fórmula: ${tableFormulas[parseInt(itmLevel)]}po</p>`);
+    } else if (roll + crit + cra.value < DC-10) {
+      toChat(message+`<h2>Falha Crítica</h2> <p>Você falha em completar o item. Você perde 10% da matéria-prima que supriu, mas pode recuperar o resto. Se quiser tentar novamente, você deve recomeçar do zero.</p>
+      <h3>Resumo</h3>
+      <p>Perda de Materiais: ${(itmValue/2)*0.1}po</p>`);    
+    } else if (roll+crit + cra.value < DC) {
+      toChat(message+`<h2>Falha</h2> <p>Falha Você falha em completar o item. Você pode recuperar a matéria-prima que supriu em seu valor total. Se quiser tentar novamente, você deve recomeçar do zero.</p>`);
+    }    
+  }
+  
+  // Form ==============================
+  let applyChanges = false;
+  new Dialog({
+    title: `Manufaturar - ${version}`,
+    content: `
+    <div>Coloque os dados do item que deseja manufaturar. Não esqueça que precisa da fórmula para poder realizar a manufatura.<div>
     <hr/>
     <form>
       <div class="form-group">
-        <label>Item level :</label>
-        <input id="lvl-item" name="lvl-item" type="number"/>
+        <label>Nível do Item :</label>
+        <input id="lvl-item" name="lvl-item" type="number" min="0" max="20"/>
       </div>
       <div class="form-group">
-        <label>Item value (in GP) :</label>
-        <input id="value-item" name="value-item" type="number"/>
+        <label>Valor do Item (em PO) :</label>
+        <input id="value-item" name="value-item" type="number" min="0"/>
       </div>
       <div class="form-group">
-        <label>Rare item ?</label>
+        <label>Item Raro ?</label>
         <input id="rare-item" name="rare-item" type="checkbox" value="rare"/>
       </div>
       <div class="form-group">
         <label>Devin's Sight ?</label>
         <input id="fortune" name="fortune" type="checkbox" value="fortune"/>
-      </div>
+      </div>   
     </form>
     `,
   buttons: {
     yes: {
       icon: "<i class='fas fa-check'></i>",
-      label: `Start crafting !`,
+      label: `Manufaturar !`,
       callback: () => applyChanges = true
     },
     no: {
       icon: "<i class='fas fa-times'></i>",
-      label: `Cancel`
+      label: `Cancelar`
     },
   },
   default: "yes",
@@ -460,16 +507,14 @@ new Dialog({
         var {cra} = actor.data.data.skills;
         var charLevel = actor.data.data.details.level.value;
         var charName = actor.data.name;
-        let itmType = html.find('[name="rare-item"]')[0].checked ? "rare" : "common" ;
+        let itmType = html.find('[name="rare-item"]')[0].checked ? "raro" : "comum" ;
         let devinSight = html.find('[name="fortune"]')[0].checked;
         let itmLevel = parseInt(html.find('[name="lvl-item"]')[0].value) || 0;
         let itmValue = parseInt(html.find('[name="value-item"]')[0].value) || 0;
 
-        console.log(itmType);
+        //console.log(itmType);
 
         if (itmLevel <= charLevel) {
-
-
           if (itmLevel >= 0 && itmLevel <= 20) {
             if (cra.rank >= 4) {
                     return rollCrafting({itmLevel, cra, charLevel, itmType, itmValue, charName, devinSight});
