b2037edf941887d2db0374de83cd1bdf49e62716
adfs <brunocaladoribeiro@yahoo.com.br>
2020-12-03 14:38:13 -0300

    teste2

--- Scripts/jitsi/jitsi-security.sh
+++ Scripts/jitsi/jitsi-security.sh
@@ -1,34 +1,37 @@
-#!/bin/sh
+﻿#!/bin/sh
 
 if [ $1 ]; then
   USER="foundry"
-  PASSWORD=$1
-  read -r CONTAUSUARIO<contausuario
+  PASSWORD="${1}"
+  
+  CONTAUSUARIO=$(head -n 1 contausuario)
+  
+  DOMINIO=$(head -n 1 dominio)  
+  export JITSI_DOMAIN=$DOMINIO  
 
-  read -r DOMINIO<dominio
-  export JITSI_DOMAIN=DOMINIO
+curl  https://gist.githubusercontent.com/noandrea/5ff6b212273af95103996c0e71f0cdf2/raw/22965f246c59bc149245554b6079db97794425bd/apeunit.test-config.js -s | \
+sed  "s/apeunit.test/$JITSI_DOMAIN/g" \
+> /etc/jitsi/meet/$JITSI_DOMAIN-config.js
 
-  echo $DOMINIO
+curl https://gist.githubusercontent.com/noandrea/5ff6b212273af95103996c0e71f0cdf2/raw/bb3e8a65582882dd3aeb4624d2522b244b949855/apeunit.test.cfg.lua -s | \
+sed  "s/apeunit.test/$JITSI_DOMAIN/g" | \
+sed  "s/JICOFO_SECRET/$(grep -e '^JICOFO_SECRET=.*' /etc/jitsi/jicofo/config | cut -d '=' -f2)/g" | \
+sed  "s/TURN_SECRET/$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})/g" \
+> /etc/prosody/conf.avail/$JITSI_DOMAIN.cfg.lua
 
-  curl  https://gist.githubusercontent.com/noandrea/5ff6b212273af95103996c0e71f0cdf2/raw/22965f246c59bc149245554b6079db97794425bd/apeunit.test-config.js -s | \
-  sed  "s/apeunit.test/$JITSI_DOMAIN/g" \
-  > /etc/jitsi/meet/$JITSI_DOMAIN-config.js
 
-  curl https://gist.githubusercontent.com/noandrea/5ff6b212273af95103996c0e71f0cdf2/raw/bb3e8a65582882dd3aeb4624d2522b244b949855/apeunit.test.cfg.lua -s | \
-  sed  "s/apeunit.test/$JITSI_DOMAIN/g" | \
-  sed  "s/JICOFO_SECRET/$(grep -e '^JICOFO_SECRET=.*' /etc/jitsi/jicofo/config | cut -d '=' -f2)/g" | \
-  sed  "s/TURN_SECRET/$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})/g" \
-  > /etc/prosody/conf.avail/$JITSI_DOMAIN.cfg.lua
+echo "org.jitsi.jicofo.auth.URL=XMPP:$JITSI_DOMAIN" \
+>> /etc/jitsi/jicofo/sip-communicator.properties
 
-  echo "org.jitsi.jicofo.auth.URL=XMPP:$JITSI_DOMAIN" \
-  >> /etc/jitsi/jicofo/sip-communicator.properties
-
-  prosodyctl register $USER $JITSI_DOMAIN $PASSWORD
+  prosodyctl register $USER $JITSI_DOMAIN $PASSWORD  
   
   touch DadosJitsi
+  echo "===================" > DadosJitsi
   echo "Usuario e Senha da Conta Jitsi" >> DadosJitsi
   echo "Usuario: ${USER}" >> DadosJitsi
   echo "Senha: ${PASSWORD}" >> DadosJitsi
+  echo "===================" >> DadosJitsi
+  cat DadosJitsi
   chown $CONTAUSUARIO.$CONTAUSUARIO DadosJitsi
   
   service jicofo restart
