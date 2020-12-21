8d5283b8fcb2dc0b377afe148acb731e2f103da4
adfs <brunocaladoribeiro@yahoo.com.br>
2020-12-20 23:53:33 -0300

    rfhrtyrtyrt

--- Scripts/foundryvtt/foundryassistentesemhttps.sh
+++ Scripts/foundryvtt/foundryassistentesemhttps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #version v1.00
-if [ $# -eq 2 ]; then
-  DOMINIO=$1
-  DOWNLOAD=$2
+if [ $# -eq 1 ]; then  
+  DOWNLOAD=$1
   
   # Instala ZIP
   echo "===== Instala ZIP ====="
@@ -18,12 +17,11 @@ if [ $# -eq 2 ]; then
   sudo chmod +x ~/foundry/resources/app/main.js    
   rm fvtt.zip
   cd ~
-  nohup ./foundry/resources/app/main.js &  
-  
+    
   echo "===== Cria Atalhos ====="  
-  ln -s .local/share/FoundryVTT/Data/ data
-  ln -s .local/share/FoundryVTT/Config/ config
-  ln -s .local/share/FoundryVTT/Logs/ logs
+  ln -s ~/.local/share/FoundryVTT/Data/ data
+  ln -s ~/.local/share/FoundryVTT/Config/ config
+  ln -s ~/.local/share/FoundryVTT/Logs/ logs
   
   # Atualizacao Inicial
   echo "===== Atualizacao Inicial ====="  
@@ -38,36 +36,8 @@ if [ $# -eq 2 ]; then
   sudo apt install -y nodejs
   sudo apt install -y build-essential
 
-  # NOIP
-  echo "===== Instala NOIP ====="
-  sudo apt -y install gcc make
-  cd /usr/local/src/
-  sudo curl -o noip-duc-linux.tar.gz http://www.noip.com/client/linux/noip-duc-linux.tar.gz
-  sudo tar xf noip-duc-linux.tar.gz
-  sudo rm noip-duc-linux.tar.gz
-  cd noip-2.1.9-1/
-  sudo make install
-  cd
-  curl -o noip https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Scripts/noip/noip.sh
-  chmod +x noip
-  ./noip start
-
-  # Certbot  
-  sudo apt -y install snapd
-  sudo snap install core
-  sudo snap refresh core
-  sudo snap install --classic certbot
-  sudo ln -s /snap/bin/certbot /usr/bin/certbot
-  sudo certbot certonly --standalone -d $DOMINIO
-  
-  # Certificado para o Foundry VTT
-  pkill node
-  sudo apt -y install acl  
-  sudo setfacl -R -m u:$(whoami):rX /etc/letsencrypt/{live,archive}/$DOMINIO  
-  sudo setfacl -m u:$(whoami):rX /etc/letsencrypt/{live,archive}  
-  # Configura os arquivos do Foundry VTT
-  sed -i 's+"sslCert": null+"sslCert": "/etc/letsencrypt/live/'$DOMINIO'/cert.pem"+g' ~/.local/share/FoundryVTT/Config/options.json
-  sed -i 's+"sslKey": null+"sslKey": "/etc/letsencrypt/live/'$DOMINIO'/privkey.pem"+g' ~/.local/share/FoundryVTT/Config/options.json
+  # Liga o Foundry VTT
+  nohup ./foundry/resources/app/main.js &
 
   # Chave para arquivos
   ssh-keygen -t rsa -f ~/.ssh/arquivoschave -C $(whoami) -N "" 
@@ -75,26 +45,22 @@ if [ $# -eq 2 ]; then
   # Dados de Consulta
   echo "Usuario: $(whoami)" > dadossuporte
   echo "Maquina: $(hostname)" >> dadossuporte    
-  echo "Endereco: ${DOMINIO}:30000" >> dadossuporte    
   echo "Local da chave publica: .ssh/arquivoschave.pub" >> dadossuporte    
   echo "Local da chave privada: .ssh/arquivoschave" >> dadossuporte    
   
   # Assistente
-  curl -o jarbas https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Scripts/foundryvtt/jarbas.sh
+  curl -o jarbas https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Scripts/foundryvtt/jarbasjarbassemhttps.sh
   chmod +x jarbas
 
   # Final
-  curl -o md https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Scripts/logo.txt
-  cat md
   echo "Comandos adicionados: jarbas, noip"
+  curl -o md https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Scripts/logo.txt
+  cat md  
 
   rm md
   rm assistente
   
-  # Mostra chaves
-  echo 
-  echo
-  ./jarbas chaves  
+  pkill node
 else
   echo "Coloque o dominio e o endereco de download do foundry vtt"
 fi
--- Scripts/foundryvtt/jarbasjarbassemhttps.sh
+++ Scripts/foundryvtt/jarbasjarbassemhttps.sh
@@ -3,40 +3,56 @@
 VERSION="v1.00"
 echo "========================================"
 case "$1" in
-    start)
+    ligar)
         echo "Iniciando o Foundry VTT"
-        pm2 start foundry/resources/app/main.js --name fvtt
+        echo "Pressione enter para continuar." 
+        nohup ./foundry/resources/app/main.js &
     ;;
-    stop)
-        echo "Encerrando o Foundry VTT"
-        pm2 stop foundry/resources/app/main.js --name fvtt
+    desligar)
+        echo "Encerrando o Foundry VTT"        
+        pkill node
     ;;
     status)
         echo "Verificando se o Foundry VTT esta executando"        
-        pm2 list all
+        cat nohup.out
+        echo
+        echo "Se 'node' aparecer, o Foundry estara rodando:"
+        pgrep -f node
     ;;  
-    version)
+    versao)
         echo "Checagem de Versoes"        
-        node --version
-        nvm --version
+        node --version        
     ;;
-    force)
+    forcar)
         echo "Encerra o FVTT, Atualiza o IP, Inicia o FVTT"        
-        pm2 stop foundry/resources/app/main.js --name fvtt
-        sudo /usr/local/bin/noip2
-        pm2 start foundry/resources/app/main.js --name fvtt
+        pkill node        
+        echo "Pressione enter para continuar." 
+        nohup ./foundry/resources/app/main.js &
+    ;;    
+    suporte)
+        echo "Dados da Maquina"        
+        cat dadossuporte
     ;;    
     chaves)
         echo "Chaves localizadas"        
         ls -la ~/.ssh
+        echo
         echo "No Google Cloud você deve colocar sua chave publica (.pub). Ao copiar a chave publica copie apenas o que esta entre os separadores"
+        echo
+        echo
         echo "===== CHAVE PUBLICA - COMECO - NAO COPIE ESSA LINHA ====="        
         cat .ssh/arquivoschave.pub
         echo "===== CHAVE PUBLICA - FIM - NAO COPIE ESSA LINHA ====="
+        echo
+        echo
         echo "No Filezilla você deve colocar sua chave privada (arquivo sem extensao). Copie o conteudo dele para um arquivo de texto e salve como chave.ppk no seu computador. "
+        echo
+        echo
         echo "===== CHAVE PRIVADA - COMECO - NAO COPIE ESSA LINHA ====="        
         cat .ssh/arquivoschave
         echo "===== CHAVE PRIVADA - FIM - NAO COPIE ESSA LINHA ====="        
+        echo
+        echo
     ;;        
     *)
         echo "Jarbas Versao ${VERSION}"
@@ -44,10 +60,11 @@ case "$1" in
         echo "Exemplo de uso: ./jarbas start"
         echo
         echo "chaves: Mostra chaves de acesso"
-        echo "start: Inicia o Foundry VTT"
-        echo "stop: Para o Foundry VTT"
+        echo "suporte: Mostra dados da maquina"
+        echo "ligar: Inicia o Foundry VTT"
+        echo "desligar: Para o Foundry VTT"
         echo "status: Verifica se o Foundry VTT está rodando"
-        echo "force: Encerra o FVTT, Atualiza o IP (noip), Inicia o FVTT"
+        echo "forcar: Encerra o FVTT, Inicia o FVTT"
         exit 1
 esac
 echo "========================================"
