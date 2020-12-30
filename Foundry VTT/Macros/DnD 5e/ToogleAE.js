aef63637bea8ba02dc8108f6ea82f376ff166d7a
adfs <brunocaladoribeiro@yahoo.com.br>
2020-12-30 11:01:07 -0300

    asdf

--- Foundry VTT/Macros/DnD 5e/ToogleAE.js
+++ Foundry VTT/Macros/DnD 5e/ToogleAE.js
@@ -1,6 +1,5 @@
 const nome = "MeuEfeito"; // Troque MeuEfeito pelo nome exato do efeito que você quer ligar ou desligar.
 
-
 const macroVersion = 'v0.1';
 
 /* Ativar/Desativar Efeito
@@ -20,6 +19,15 @@ async function main() {
   if (!effect) {
     ui.notifications.warn(`Efeito não localizado!`); // get selected token 
   } else {
-    await effect.update({disabled: !effect.data.disabled});      
+    await effect.update({disabled: !effect.data.disabled});            
+    let mensagem = `O efeito <b>${nome}</b> foi <b>${effect.data.disabled?'ligado':'desligado'}</b>`;
+    let chatData = {
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker(),
+      content: mensagem
+    };  
+    ChatMessage.create(chatData, {});    
+    
   }
 }
+
