361f8856e549922974a96f65013b0d1d05fa96e3
adfs <brunocaladoribeiro@yahoo.com.br>
2021-03-12 10:53:49 -0300

    asdfasdf

--- Foundry VTT/Macros/Sistemas Diversos/TerraDevastada.js
+++ Foundry VTT/Macros/Sistemas Diversos/TerraDevastada.js
@@ -1,6 +1,6 @@
 //onde em 1D6 contam os pares como sucessos e o número 6 do dado explode!
 
-const macroVersion = 'v0.2';
+const macroVersion = 'v0.3';
 /* Terra Devastada
 ## Features
 - dice so nice
@@ -16,6 +16,7 @@ let dice = 1; // coloque a quantidade de dados que deseja usar
   let roll3d;
   let roll;
   let message = '';
+  let explosoes=0;
   
   for (var i = 0; i < truedice; i++) {
     console.log(i);
@@ -27,14 +28,23 @@ let dice = 1; // coloque a quantidade de dados que deseja usar
       sucessos+=1;
       if(roll==6){
         truedice+=1;
+        explosoes+=1;
       }
     }
     
   } // fim for
 
-  message+=`<h2 style="color:Red">Terra Devastada</h2>`;
-  message+=`<p>Você rolou ${dice} dado(s) e teve <b style="color:Red">${sucessos}</b> sucessos.</p>`;
-  
+  message+=`<h2 style="color:Red">Terra Devastada</h2>`;  
+  message+=`<p>Você rolou ${dice} dado(s), e teve <b style="color:Blue">${explosoes}</b> explosões.</p>`;
+  message += `<table>
+<tbody>
+<tr>
+<td><img style="vertical-align:middle" src="https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Foundry%20VTT/Macros/Sistemas%20Diversos/TerraDevastada.webp" width="32" height="32"></td>
+<td><p>Você teve <b style="color:Red">${sucessos}</b> sucessos.</p></td>
+</tr>
+</tbody>
+</table>`;
+
   let chatData = {
     user: game.user._id,    
     content: message,
