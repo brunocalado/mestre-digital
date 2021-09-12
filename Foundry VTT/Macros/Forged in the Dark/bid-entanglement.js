d26b1d6581b8060d35c8fd31c58f34d3bf4cb88b
Bruno Calado <brunocalado@users.noreply.github.com>
2021-09-12 17:08:00 -0300

    asdf

--- Foundry VTT/Macros/Forged in the Dark/bid-entanglement.js
+++ Foundry VTT/Macros/Forged in the Dark/bid-entanglement.js
@@ -1,13 +1,16 @@
 const tableHeatA = 'Entanglements - Heat 0-3';  // Entanglements - Heat 0-3
 const tableHeatB = 'Entanglements - Heat 4/5';  // Entanglements - Heat 4/5
 const tableHeatC = 'Entanglements - Heat 6+';   // Entanglements - Heat 6+
-let crewName = 'crew';  
-const version = '1.0';
+const compendiumTables = 'Entanglements';       // Compendium name 
+const version = '1.1';
 main();
 
 /*
 TODO
 - replace text by journal
+
+source: https://raw.githubusercontent.com/brunocalado/mestre-digital/master/Foundry%20VTT/Macros/Forged%20in%20the%20Dark/bid-entanglement.js
+icon: icons/magic/nature/root-vines-entwined-leaves.webp
 */
   
 async function main() {
@@ -25,6 +28,10 @@ async function main() {
     content: `
     <h2>Choose</h2>
     <p><b>Crew:</b> <select id="playerName" style="width: 200px">${playerNameList}</select></p>
+    <br>
+    <p>
+      <input type="checkbox" id="whisper" checked/>Whisper?
+    </p>
     `,
     buttons: {
       roll: {
@@ -42,7 +49,8 @@ async function main() {
 
 async function createImageFolder(html) {
   let playerName = html.find("#playerName")[0].value;
-  
+  let whisper = html.find("#whisper")[0].checked;
+
   let crew = game.actors.getName(playerName).data.data;
   let crewHeat = parseInt(crew.heat);
   let crewWanted = parseInt(crew.wanted);
@@ -54,17 +62,17 @@ async function createImageFolder(html) {
     case 1:
     case 2:
     case 3:
-      entanglementToUse = game.tables.getName(tableHeatA);
+      entanglementToUse = await drawFromTable(tableHeatA, compendiumTables) 
     break
     case 4:
     case 5:
-      entanglementToUse = game.tables.getName(tableHeatB);
+      entanglementToUse = await drawFromTable(tableHeatB, compendiumTables) 
     break
     case 6:
     case 7:
     case 8:
     case 9:
-      entanglementToUse = game.tables.getName(tableHeatC);
+      entanglementToUse = await drawFromTable(tableHeatC, compendiumTables) 
   }
 
   let rollFormula = `${crewWanted}d6`;
@@ -73,7 +81,23 @@ async function createImageFolder(html) {
   }
 
   entanglementToUse.data.formula = rollFormula;
-
-  entanglementToUse.draw();
+  
+  if (whisper) {
+    entanglementToUse.draw({rollMode: 'gmroll'});
+  } else {
+    entanglementToUse.draw();
+  }  
 }
 
+async function drawFromTable(tableName, compendiumName) {
+  let list_compendium = await game.packs.filter(p=>p.documentName=='RollTable');      
+  let inside = await list_compendium.filter( p=>p.metadata.label==compendiumName )[0].getDocuments();      
+  let table = await inside.filter( p=>p.data['name']==tableName )[0];
+  
+  if (!table) {
+    ui.notifications.warn(`Table ${tableName} not found.`, {});
+    return;
+  }
+  //return await table.draw({rollMode: 'gmroll'});
+  return await table;
+}
