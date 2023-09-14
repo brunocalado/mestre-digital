c2385af584a3131108a9fa413b32085b92174f50
Bruno Calado <brunocalado@users.noreply.github.com>
2023-09-14 11:31:03 -0300

    111

--- Scripts/oraclecloud/jarbas - Copia (2).sh
+++ Scripts/oraclecloud/jarbas - Copia (2).sh
@@ -1,7 +1,7 @@
 #######################################################
 ## jarbas Oracle Cloud ################################
 #! /bin/sh
-VERSION="v1.43"
+VERSION="v1.42"
 echo "========================================"
 case "$1" in
     login2left)
@@ -113,18 +113,10 @@ case "$1" in
         ./jarbas desligar     
         # https://github.com/nodesource/distributions
         # Using Ubuntu
-        sudo apt-get update
-        sudo apt-get install -y ca-certificates curl gnupg
-        sudo mkdir -p /etc/apt/keyrings
-        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg        
-        
-        NODE_MAJOR=18
-        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
- 
-        sudo apt-get update
-        sudo apt-get install nodejs -y 
+        curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - 
+        sudo apt update
+        sudo apt-get install -y nodejs        
         sudo apt update && sudo apt -y upgrade
-        
         ./jarbas ligar     
         sudo reboot
         echo "======================"        
