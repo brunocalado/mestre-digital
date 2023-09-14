f0484a9e2717076a97d6013be61fc8f55f441595
Bruno Calado <brunocalado@users.noreply.github.com>
2023-09-14 11:37:00 -0300

    d

--- Scripts/oraclecloud/foundryassistenteoraclecloud.sh
+++ Scripts/oraclecloud/foundryassistenteoraclecloud.sh
@@ -34,7 +34,15 @@ if [ $# -eq 1 ]; then
   # Instala NODE
   echo "===== Instala NODE e Gerenciador de Processos ====="
   cd ~
-  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - 
+  
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl gnupg
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg        
+  
+  NODE_MAJOR=18
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  
   sudo apt-get install -y nodejs
   
   # Auto inicia com a vm
