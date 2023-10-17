3b595b5e57a145a77f82c00a29a9a2e7939775d3
Bruno Calado <brunocalado@users.noreply.github.com>
2023-10-17 15:59:37 -0300

    sadf

--- Scripts/oraclecloud/foundryassistenteoraclecloud.sh
+++ Scripts/oraclecloud/foundryassistenteoraclecloud.sh
@@ -43,6 +43,7 @@ if [ $# -eq 1 ]; then
   NODE_MAJOR=18
   echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
   
+  sudo apt update
   sudo apt-get install -y nodejs
   
   # Auto inicia com a vm
