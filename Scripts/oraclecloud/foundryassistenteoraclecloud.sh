bbc35342eaea7ee7641e0ee3da3f53b8babbbd30
Bruno Calado <brunocalado@users.noreply.github.com>
2025-04-03 16:11:46 -0300

    node

--- Scripts/oraclecloud/foundryassistenteoraclecloud.sh
+++ Scripts/oraclecloud/foundryassistenteoraclecloud.sh
@@ -40,13 +40,16 @@ if [ $# -eq 1 ]; then
   echo "===== Instala NODE e Gerenciador de Processos ====="
   cd ~
   
-  sudo apt-get update
-  sudo apt-get install -y ca-certificates curl gnupg
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg        
+  sudo apt-get update        
+  sudo apt-get install -y curl
+  curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+  sudo -E bash nodesource_setup.sh
+  #sudo apt-get install -y ca-certificates curl gnupg
+  #sudo mkdir -p /etc/apt/keyrings
+  #curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg        
   
-  NODE_MAJOR=20
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  #NODE_MAJOR=20
+  #echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
   
   sudo apt update
   sudo apt-get install -y nodejs
